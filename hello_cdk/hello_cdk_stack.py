8923f77bc701cf839d18addade5ddf28f051ff00
xion1358 <xion1358@stthomas.edu>
2024-10-03 20:57:06 +0000

    Added removal and auto delete s3 bucket

--- hello_cdk/hello_cdk_stack.py
+++ hello_cdk/hello_cdk_stack.py
@@ -15,7 +15,11 @@ class HelloCdkStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
-        bucket = s3.Bucket(self, "MyFirstBucket", versioned=True)
+        bucket = s3.Bucket(self, 
+                            "MyFirstBucket", 
+                            versioned=True,
+                            removal_policy=cdk.RemovalPolicy.DESTROY,
+                            auto_delete_objects=True)
         # example resource
         # queue = sqs.Queue(
         #     self, "HelloCdkQueue",
