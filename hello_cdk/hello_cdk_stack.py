cdb8a79834bcedd05fbe553d10b3a113d8fdc45f
EC2 Default User <ec2-user@ip-10-0-1-24.ec2.internal>
2024-10-03 20:16:11 +0000

    add S3 buckets

--- hello_cdk/hello_cdk_stack.py
+++ hello_cdk/hello_cdk_stack.py
@@ -1,17 +1,21 @@
 from aws_cdk import (
     # Duration,
     Stack,
+    aws_s3 as s3
     # aws_sqs as sqs,
 )
+
+import aws_cdk as cdk
+
 from constructs import Construct
 
 class HelloCdkStack(Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: cdk.App, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
-
+        bucket = s3.Bucket(self, "MyFirstBucket", versioned=True)
         # example resource
         # queue = sqs.Queue(
         #     self, "HelloCdkQueue",
